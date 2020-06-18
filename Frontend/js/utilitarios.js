0dcfe07fad8b2792f3ae5d553ce95bc8b9cbe611
Thiago Damaceno <tavaresdamaceno@gmail.com>
2020-06-18 15:26:51 -0300

    Adicionando função que embaralhas os itens de um array

--- Frontend/js/utilitarios.js
+++ Frontend/js/utilitarios.js
@@ -1,3 +1,19 @@
 function randomInt(min, max) {
   return min + Math.floor((max - min) * Math.random());
 }
+
+function embaralharArray(array) {
+  var indice_atual = array.length, valor_temporario, indice_aleatorio;
+
+  while (0 !== indice_atual) {
+
+    indice_aleatorio = Math.floor(Math.random() * indice_atual);
+    indice_atual -= 1;
+
+    valor_temporario = array[indice_atual];
+    array[indice_atual] = array[indice_aleatorio];
+    array[indice_aleatorio] = valor_temporario;
+  }
+
+  return array;
+}
